e2eaa9c5bee255ac7d1c52b8590c696008077121
Andarael <60547428+Andarael@users.noreply.github.com>
2021-06-12 13:52:31 +0200

    Added comments and keep_lod flag to some methods

--- AutoLOD_addon.py
+++ AutoLOD_addon.py
@@ -1,3 +1,7 @@
+# Blender Addon by Josué Raad, for version 2.8+
+
+# ---------- ---------- Imports ---------- ----------
+
 import bpy
 import math
 from bpy.props import PointerProperty
@@ -18,13 +22,15 @@ bl_info = {
 
 # ---------- ---------- Global definitions ---------- ----------
 
+# The name that the decimate modifier will use for LODs
+# I use a specific name, the the addon does not have to store every modifiers for every objects
 lod_modifier_name = "lod_modifier"
 
 # ---------- ---------- Properties ---------- ----------
 
-# create a property in the scene. Later i have an interface to draw it. todo
-# And in the add modifier ops I check if the prop is not null
+# definitions of the addon preperties
 
+# Since Blender 2.79 it is possible to use a pointer property to select objects
 bpy.types.Scene.lod_target = PointerProperty(
     type=bpy.types.Object, name="lod target", description="The object to compute lod distance from")
 
@@ -40,9 +46,13 @@ bpy.types.Scene.lod_start_dist = bpy.props.FloatProperty(
 bpy.types.Scene.lod_ratio_multiplier = bpy.props.FloatProperty(
     name="lod ratio multiplier", description="Decimation ratio multiplier", default=1, min=0, max=1, subtype='FACTOR')
 
+# LODs level above 6 seems useless :
+# if someone ever needs more than 6, this means that either the model is not ready for production
+# or that a firest decimate modifier should be applyed first
 bpy.types.Scene.lod_max_level = bpy.props.IntProperty(
     name="lod max level", description="The maximum level of lod", default=6, min=0)
 
+# Used to track if the object is a LOD, so that GLOBAL operators affect the object even if it is not selected
 bpy.types.Object.is_lod = bpy.props.BoolProperty(
     name="is lod", description="is the object a lod", default=False)
 
@@ -51,13 +61,15 @@ bpy.types.Object.is_lod = bpy.props.BoolProperty(
 
 def get_distance(object1, object2):
     """compute the distance between 2 objects"""
+    if object1 is None or object2 is None:
+        return 0.0
     return (object1.location - object2.location).length
 
 # ----------
 
 
 def objects_dont_share_data(objects):
-    """returns True if no selcted objects shares the same data"""
+    """returns True if no objects in the list shares the same data"""
     for object in objects:
         if object.data.users != 1:
             return False
@@ -78,24 +90,33 @@ def get_all_lod_objects():
 # ---------- ---------- Utils fuinction for Operators ---------- ----------
 
 
-def remove_all_lods(objects):
-    """iterates on the selected objects and removes the lod modifier"""
+def remove_all_lods(objects, keep_lod_flag=False):
+    """iterates on the selected objects and removes the lod modifier
+    If keep_lod is true, then the lods flags for the objects are left unchanged
+    """
     for object in objects:
-        remove_decimate(object)
+        remove_decimate(object, keep_lod_flag)
 
 # ----------
 
 
-def remove_decimate(object):
+def remove_decimate(object, keep_lod_flag=False):
+    """Removes the decimate modifier from the object
+    If keep_lod_flag is true, then the lod flag for the object is left unchanged
+    """
     old_modifier = object.modifiers.get(lod_modifier_name)
     if old_modifier is not None:
         object.modifiers.remove(old_modifier)
-    object.is_lod = False
+    if not keep_lod_flag:
+        object.is_lod = False
 
 # ----------
 
 
 def get_lod_target():
+    """Returns the lod toarget for the current scene.
+    I use a method to get it in case the target is None, then the default camera is used
+    """
     scene = bpy.context.scene
 
     if scene.lod_target is None:
@@ -108,33 +129,37 @@ def get_lod_target():
 
 
 def add_decimate_modifiers(objects):
-    """Add a decimate modifier, the decimation depends on the computed lod level"""
+    """Add a decimate modifier to all objecst in the list"""
     target = get_lod_target()
     for object in objects:
-        add_decimate_single(object, target)  # then add a new one
+        add_decimate_single(object, target)
 
 # ----------
 
 
 def add_decimate_single(object, target):
-    remove_decimate(object)  # we first remove old lod modifiers
+    """Add a decimate modifier to an object
+    the decimation depends on the computed lod level
+    """
+    remove_decimate(object, True)  # we first remove old lod modifiers
 
     lod_modifier = object.modifiers.new(lod_modifier_name, "DECIMATE")
 
-    if (lod_modifier is None):  # we can't add lod to the object (not a mesh)
+    if (lod_modifier is None):  # we cannot add lod to the object (not a mesh)
         return None
 
     lod_level = get_lod_level(get_distance(object, target))
     ratio = bpy.context.scene.lod_ratio_multiplier
     set_lod_decimation(lod_modifier, lod_level, ratio)
     object.is_lod = True
+
     return lod_modifier
 
 # ----------
 
 
 def set_lod_decimation(lod_modifier, lod_level, ratio):
-    """set the decimation based on the lod level"""
+    """set the decimation of the modifier based on the lod level"""
     if lod_level > 0:  # only lods level above 0 create lods
         # here we compute the actual decimation from the lod level.
         lod_modifier.ratio = 1 / 2**lod_level * ratio
@@ -143,14 +168,14 @@ def set_lod_decimation(lod_modifier, lod_level, ratio):
 
 
 def get_lod_level(distance):
-    """compute the LOD level based on the distance and Scene properties"""
+    """computes the LOD level based on a distance and Scene properties"""
     scene = bpy.context.scene
     lod_bias = scene.lod_bias
     start_dist = scene.lod_start_dist
     max_level = scene.lod_max_level
     agressivity = scene.agressivity
 
-    distance -= start_dist
+    distance -= start_dist  # The actual distance used for compute begins at Start_dist
 
     if distance < 0 or agressivity == 0:  # if the object is bellow the min distance or lods are disabled
         return -1
@@ -170,11 +195,15 @@ def get_lod_level(distance):
 
 
 def apply_all_lods(objects):
-    backup_active = bpy.context.object
+    """Apply the LOD modifier for all objects in the list"""
+
+    backup_active = bpy.context.object  # used to restore the active object after the operation
+
     for object in objects:
         bpy.context.view_layer.objects.active = object
         bpy.ops.object.modifier_apply(modifier=lod_modifier_name)
         object.is_lod = False
+
     bpy.context.view_layer.objects.active = backup_active
 
 # ----------
@@ -184,35 +213,33 @@ def apply_all_lods(objects):
 
 
 class ExportLodOperatorObj(bpy.types.Operator):
-    """Exports Lods for the selected object, in .obj format"""
+    """Exports Lods for the selected object, in .obj format up to the MAX_LOD_LEVEL"""
     bl_idname = "object.export_lod_obj"
     bl_label = "Export LOD (.obj)"
     bl_option = {'REGISTER', 'UNDO'}
 
     @classmethod
     def poll(cls, context):
-        return context.active_object is not None
+        return (context.active_object is not None) and (context.active_object.type == 'MESH')
 
     def execute(self, context):
         active_object = context.object
 
-        if active_object.type != 'MESH':
-            return
-
-        is_lod = active_object.is_lod
+        is_lod = active_object.is_lod  # original lod flag will be restored at the end of the operation
 
         lod_max_level = context.scene.lod_max_level
         lod_modifier = add_decimate_single(active_object, active_object)
 
         for i in range(lod_max_level + 1):
             set_lod_decimation(lod_modifier, i, 1)
-            path = bpy.path.abspath("//") + active_object.name + "_lod" + str(i) + ".obj"
-            bpy.ops.export_scene.obj(filepath=path, use_selection=True, use_mesh_modifiers=True)
+            path = bpy.path.abspath("//") + active_object.name + "_lod" + str(i) + ".obj"  # filename creation
+            bpy.ops.export_scene.obj(filepath=path, use_selection=True, use_mesh_modifiers=True)  # export operator
 
+        # lod restoration
         if is_lod:
-            add_decimate_single(active_object, get_lod_target())
+            add_decimate_single(active_object, get_lod_target())  # a side effect is that the lod will be updated.
         else:
-            remove_decimate(active_object)
+            remove_decimate(active_object, False)
 
         return {'FINISHED'}
 
@@ -220,7 +247,7 @@ class ExportLodOperatorObj(bpy.types.Operator):
 # ----------
 
 class ApplyAllLodOperator(bpy.types.Operator):
-    """Apply LODs for all objects that have LODs (apply lods for all lod objects)"""
+    """Apply LODs for all objects that have LODs"""
     bl_idname = "object.apply_all_lod"
     bl_label = "Apply All LODs (!)"
     bl_option = {'REGISTER', 'UNDO'}
@@ -271,7 +298,7 @@ class RemoveLodOperator(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context):
-        remove_all_lods(context.selected_objects)
+        remove_all_lods(context.selected_objects, True)
         bpy.ops.ed.undo_push()
         return {'FINISHED'}
 
@@ -290,7 +317,7 @@ class RemoveAllLodOperator(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context):
-        remove_all_lods(get_all_lod_objects())
+        remove_all_lods(get_all_lod_objects(), False)
         bpy.ops.ed.undo_push()
         return {'FINISHED'}
 
@@ -342,6 +369,7 @@ class SetLodOperator(bpy.types.Operator):
 # ---------- ---------- UI ---------- ----------
 
 def draw_lod_panel(scene, layout):
+    """Draws a LOD panel in the given layout"""
 
     draw_properties(scene, layout)
 
@@ -352,7 +380,7 @@ def draw_lod_panel(scene, layout):
     draw_global_ops(layout)
 
     layout.separator()
-    draw_export_op(layout)
+    draw_export_ops(layout)
 
 # ----------
 
@@ -403,7 +431,7 @@ def draw_global_ops(layout):
 # ----------
 
 
-def draw_export_op(layout):
+def draw_export_ops(layout):
     layout.label(text="Export :")
 
     box = layout.box()
@@ -416,6 +444,7 @@ def draw_export_op(layout):
 
 
 class LOD_PT_lod_panel_scene(bpy.types.Panel):
+    """Draws the LOD panel in the scene properties"""
     bl_label = "LODs"
     bl_category = "LOD Panel"
     bl_space_type = 'PROPERTIES'
@@ -429,6 +458,7 @@ class LOD_PT_lod_panel_scene(bpy.types.Panel):
 
 
 class LOD_PT_lod_panel_view_3D(bpy.types.Panel):
+    """Draws the LOD panel in the 3D view"""
     bl_label = "LODs"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
